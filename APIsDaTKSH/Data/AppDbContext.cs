8b9bea713684271ad00a273ca6a1825828f3549c
Hugo Adriano de Oliveira <hugoadrianodeoliveira1@gmail.com>
2024-01-27 12:56:44 -0400

    g

---
+++ APIsDaTKSH/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace APIsDaTKSH.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+
+        }
+    }
+}
